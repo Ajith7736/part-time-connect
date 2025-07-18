4dbb61648f9ea0e82d59a2f320cacbeebedf2200
Ajith7736 <ajith.aju39502@gmail.com>
2025-07-18 19:50:03 +0530

    Update Login.jsx

--- components/Company/Login.jsx
+++ components/Company/Login.jsx
@@ -67,7 +67,7 @@ function CompanyLogin() {
   return (
     <div className='h-screen bg-gray-100 flex justify-center items-center'>
      {isSubmitting && <div className='text-center fixed w-full'><Loading/></div>} <>
-     <div className='bg-white shadow-lg w-[90vw] h-[80vh] md:w-[80vw] lg:w-[40vw]  rounded-2xl px-4 flex flex-col gap-10 justify-center'>
+     <div className='bg-white shadow-lg w-[100vw] h-[80vh] md:w-[80vw] lg:w-[40vw]  rounded-2xl px-4 flex flex-col gap-10 justify-center'>
         <h1 className='text-center text-4xl logo text-purple-600'>COMPANY <br /> LOGIN</h1>
         <form action="" className='flex flex-col gap-5' onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="Email" className='font-medium text-xl'>Email</label>
