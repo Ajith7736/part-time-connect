000f8acf23ab4dbf15c31a07abf7bb804b20dd01
Ajith7736 <ajith.aju39502@gmail.com>
2025-08-05 13:18:50 +0530

    Update Sidebar.jsx

--- components/Admin/Sidebar.jsx
+++ components/Admin/Sidebar.jsx
@@ -16,7 +16,7 @@ function Sidebar() {
     }
 
     return (
-        <div className='bg-gray-950 min-h-screen h-auto w-[10vw] md:w-[15vw] lg:w-[20vw] pt-30 lg:pt-20 flex flex-col gap-6 lg:gap-8 items-center'>
+        <div className='bg-gray-950 min-h-screen h-auto w-[13vw] md:w-[15vw] lg:w-[20vw] pt-30 lg:pt-20 flex flex-col gap-6 lg:gap-8 items-center'>
             <div className='text-white hidden lg:block font-bold text-3xl'>Admin Panel</div>
             <div className='bg-gray-800 max-[450px]:w-9 w-12 lg:w-[15vw] flex  p-2 rounded-full lg:rounded-md cursor-pointer justify-center'>
                 <NavLink to={"/admin/dashboard"} className={e => { return e.isActive ? "text-white flex items-center lg:gap-5 lg:w-[80%]" : "text-gray-300 flex items-center lg:gap-5 lg:w-[80%]" }}> <div><MdDashboard className='size-8 max-[450px]:size-5'/></div><div className='hidden lg:block text-xl font-bold'>Dasboard</div></NavLink>
